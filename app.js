b9495c2dd99a7ecb750602091f10cd400f086f8d
lee <jiyangli@163.com>
2016-05-23 00:05:13 +0800

    添加angular课程内容

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require("express");
 var app = express();
-app.use(express.static("example"));
+app.use(express.static("test"));
+var data = ["Tony","lucy","lily"];
+app.get("/getData",function(req,res){
+	console.log(req.query);
+	res.send(data);
+})
+app.get("/insert",function(req,res){
+	var username = req.query.username;
+	console.log(req.query);
+	data.push(username);
+	res.send(data);
+})
 app.listen(3000);
 console.log("服务器已经启动！")
